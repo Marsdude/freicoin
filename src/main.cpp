648c3e6e5f995dda1626abb5e7326471f5cf4603
Marsdude <yanlauzon@hotmail.com>
2013-09-05 02:50:04 -0400

    second compile debug
    we're pretty much going with educated guesses, and compiling on another
    machine at this point.

--- src/main.cpp
+++ src/main.cpp
@@ -2763,7 +2763,7 @@ bool CBlock::CheckBlock(CValidationState &state, bool fCheckPOW, bool fCheckMerk
     }
 
     // Check proof of work matches claimed amount
-    if (fCheckPOW && !CheckProofOfWork(GetHash(), nBits))
+    if (fCheckPOW && !CheckProofOfWork(nHeight))
         return state.DoS(50, error("CheckBlock() : proof of work failed"));
 
     // Check timestamp
